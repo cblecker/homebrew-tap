5d287f41aa4f574b76be6baa01cf353cc27e62ab
Christoph Blecker <admin@toph.ca>
2019-05-08 10:20:41 -0700

    uhc: 0.1.11

---
+++ Formula/uhc.rb
@@ -0,0 +1,30 @@
+class Uhc < Formula
+  desc "Unified Hybrid Cloud provisioning tool"
+  homepage "https://www.openshift.com/"
+  url "https://github.com/openshift-online/uhc-cli.git",
+      :tag      => "v0.1.11",
+      :revision => "db3ffd43be488fca6d1110679351db0be709e6fd"
+  head "https://github.com/openshift-online/uhc-cli.git"
+
+  depends_on "dep" => :build
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    dir = buildpath/"src/github.com/openshift-online/uhc-cli"
+    dir.install buildpath.children - [buildpath/".brew_home"]
+
+    cd dir do
+      system "dep", "ensure", "-vendor-only"
+      system "go", "build", "-o", "#{bin}/uhc", "./cmd/uhc"
+
+      # Install bash completion
+      output = Utils.popen_read("#{bin}/uhc completion")
+      (bash_completion/"uhc").write output
+    end
+  end
+
+  test do
+    assert_match /^#{version}/, shell_output("#{bin}/uhc version")
+  end
+end
