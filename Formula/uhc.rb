94bd9ddec2c63b9245c22f1f692a350a12714600
Christoph Blecker <admin@toph.ca>
2019-08-14 10:16:40 -0700

    uhc: use modules for dependencies

--- Formula/uhc.rb
+++ Formula/uhc.rb
@@ -6,16 +6,15 @@ class Uhc < Formula
       :revision => "03cc396eb52fd2e0b224ca56e1f6995e5202a775"
   head "https://github.com/openshift-online/uhc-cli.git"
 
-  depends_on "dep" => :build
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"] = "on"
     dir = buildpath/"src/github.com/openshift-online/uhc-cli"
     dir.install buildpath.children - [buildpath/".brew_home"]
 
     cd dir do
-      system "dep", "ensure", "-vendor-only"
       system "go", "build", "-o", "#{bin}/uhc", "./cmd/uhc"
 
       # Install bash completion
