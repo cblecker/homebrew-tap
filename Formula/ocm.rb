180bc2c6c3ff5b05c3ac68e371e26532201bf7a0
Christoph Blecker <admin@toph.ca>
2019-08-27 08:56:37 -0700

    Rename uhc to ocm

---
+++ Formula/ocm.rb
@@ -0,0 +1,26 @@
+class Ocm < Formula
+  desc "CLI for the Red Hat OpenShift Cluster Manager"
+  homepage "https://www.openshift.com/"
+  url "https://github.com/openshift-online/ocm-cli.git",
+      :tag      => "v0.1.20",
+      :revision => "14a394732840e13977dec5c09ba36c1b4e902e40"
+  head "https://github.com/openshift-online/ocm-cli.git"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GO111MODULE"] = "on"
+    ENV["CGO_ENABLED"] = "0"
+
+    # Build binary
+    system "go", "build", "-o", "#{bin}/ocm", "./cmd/ocm"
+
+    # Install bash completion
+    output = Utils.popen_read("#{bin}/ocm completion")
+    (bash_completion/"ocm").write output
+  end
+
+  test do
+    assert_match /^#{version}/, shell_output("#{bin}/ocm version")
+  end
+end
