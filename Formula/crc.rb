a925872580bad9c4179d07ae434f5d9a3cb48f64
Christoph Blecker <admin@toph.ca>
2019-08-27 08:59:32 -0700

    crc 1.0.0-beta.3

--- Formula/crc.rb
+++ Formula/crc.rb
@@ -2,27 +2,22 @@ class Crc < Formula
   desc "Minimal OpenShift 4 cluster on your local machine"
   homepage "https://www.openshift.com/"
   url "https://github.com/code-ready/crc.git",
-      :tag      => "1.0.0-beta.2",
-      :revision => "158cb9d37a5f034b5a05f45b7a69d477c4bd95dc"
-  version "1.0.0-beta.2"
+      :tag      => "1.0.0-beta.3",
+      :revision => "e97bf65f1cac9700bfcb590c1d8b442e243b1144"
+  version "1.0.0-beta.3"
   head "https://github.com/code-ready/crc.git"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
     ENV["GO111MODULE"] = "on"
-    dir = buildpath/"src/github.com/code-ready/crc"
-    dir.install buildpath.children - [buildpath/".brew_home"]
 
-    cd dir do
-      os = `go env GOOS`.strip
-      arch = `go env GOARCH`.strip
-      os = "macos" if os == "darwin"
-      system "make", "out/#{os}-#{arch}/crc", "CRC_VERSION=#{version}"
+    os = `go env GOOS`.strip
+    arch = `go env GOARCH`.strip
+    os = "macos" if os == "darwin"
+    system "make", "out/#{os}-#{arch}/crc", "CRC_VERSION=#{version}"
 
-      bin.install "out/#{os}-#{arch}/crc"
-    end
+    bin.install "out/#{os}-#{arch}/crc"
   end
 
   test do
