4d3e2de34459b95ea14ea848adb5913d23ada0b5
Christoph Blecker <admin@toph.ca>
2019-08-27 09:11:24 -0700

    kind: simplify install

--- Formula/kind.rb
+++ Formula/kind.rb
@@ -8,24 +8,19 @@ class Kind < Formula
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
     ENV["GO111MODULE"] = "on"
-    dir = buildpath/"src/sigs.k8s.io/kind"
-    dir.install buildpath.children - [buildpath/".brew_home"]
 
-    cd dir do
-      system "go", "build", "-o", bin/"kind"
+    system "go", "build", "-o", bin/"kind"
 
-      prefix.install_metafiles
+    prefix.install_metafiles
 
-      # Install bash completion
-      output = Utils.popen_read("#{bin}/kind completion bash")
-      (bash_completion/"kind").write output
+    # Install bash completion
+    output = Utils.popen_read("#{bin}/kind completion bash")
+    (bash_completion/"kind").write output
 
-      # Install zsh completion
-      output = Utils.popen_read("#{bin}/kind completion zsh")
-      (zsh_completion/"_kind").write output
-    end
+    # Install zsh completion
+    output = Utils.popen_read("#{bin}/kind completion zsh")
+    (zsh_completion/"_kind").write output
   end
 
   test do
