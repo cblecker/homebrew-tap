794b74828148588eab8d8dca4afca4dab4544d8c
Christoph Blecker <admin@toph.ca>
2019-09-17 13:30:43 -0700

    crc 1.0.0-beta.5

--- Formula/crc.rb
+++ Formula/crc.rb
@@ -2,9 +2,9 @@ class Crc < Formula
   desc "Minimal OpenShift 4 cluster on your local machine"
   homepage "https://www.openshift.com/"
   url "https://github.com/code-ready/crc.git",
-      :tag      => "1.0.0-beta.4",
-      :revision => "4f801d28d1af0318df9a88a5d3db66874a300e8c"
-  version "1.0.0-beta.4"
+      :tag      => "1.0.0-beta.5",
+      :revision => "f2aa58c1e72a75e4ac24529923bf33ce7bc67c17"
+  version "1.0.0-beta.5"
   head "https://github.com/code-ready/crc.git"
 
   depends_on "go" => :build
