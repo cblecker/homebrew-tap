5c14480feb45b665b3c60576543a23f96b1b1f72
Christoph Blecker <admin@toph.ca>
2021-11-28 14:15:46 -0800

    ocm-backplane 0.0.15

--- Formula/ocm-backplane.rb
+++ Formula/ocm-backplane.rb
@@ -2,8 +2,8 @@ class OcmBackplane < Formula
   desc "CLI for interacting with the IMS Backplane"
   homepage "https://www.openshift.com/"
   url "https://gitlab.cee.redhat.com/service/backplane-cli.git",
-      :tag      => "0.0.14",
-      :revision => "d27d676ea36dc9eda6b6c613d2f737e4c9a642c4"
+      :tag      => "0.0.15",
+      :revision => "1d33b24eeca7d0bf6064233f45d1bc993a6d8f3a"
   head "https://gitlab.cee.redhat.com/service/backplane-cli.git"
 
   depends_on "go" => :build
