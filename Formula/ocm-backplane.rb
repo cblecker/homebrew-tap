254ec36c75dd066cce78bab4a20e5beda3d7f421
Christoph Blecker <admin@toph.ca>
2022-09-16 11:05:52 -0700

    ocm-backplane 0.0.29

--- Formula/ocm-backplane.rb
+++ Formula/ocm-backplane.rb
@@ -2,8 +2,8 @@ class OcmBackplane < Formula
   desc "CLI for interacting with the IMS Backplane"
   homepage "https://www.openshift.com/"
   url "https://gitlab.cee.redhat.com/service/backplane-cli.git",
-      tag:      "0.0.28",
-      revision: "4f0ce14a7a2ab8d725ed3615eb81c55a5e0b2aa2"
+      tag:      "0.0.29",
+      revision: "46bbbbae195a1f784bf20619d8fa96282630ea65"
   head "https://gitlab.cee.redhat.com/service/backplane-cli.git"
 
   livecheck do
@@ -22,5 +22,26 @@ class OcmBackplane < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/ocm-backplane version")
+    (testpath/"kubeconfig").write(<<~EOF)
+      apiVersion: v1
+      clusters:
+      - cluster:
+          server: https://cluster.example:443
+        name: cluster-example:443
+      contexts:
+      - context:
+          cluster: cluster-example:443
+          namespace: default
+          user: test-user/cluster-example:443
+        name: default/cluster-example:443/test-user
+      current-context: default/cluster-example:443/test-user
+      kind: Config
+      preferences: {}
+      users:
+      - name: test-user/cluster-example:443
+        user:
+          token: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    EOF
+    assert_match "the api server is not a backplane url", shell_output("KUBECONFIG=#{testpath}/kubeconfig #{bin}/ocm-backplane status 2>&1", 1)
   end
 end
