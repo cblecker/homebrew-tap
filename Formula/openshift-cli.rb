fa49de34f000d6b9d87d1684bded020f0326ef3a
Christoph Blecker <admin@toph.ca>
2022-10-11 08:49:22 -0400

    openshift-cli 4.11.7

--- Formula/openshift-cli.rb
+++ Formula/openshift-cli.rb
@@ -3,9 +3,9 @@ class OpenshiftCli < Formula
   homepage "https://www.openshift.com/"
   url "https://github.com/openshift/oc.git",
       shallow:  false,
-      # tag: => "v4.11.6", # oc adm release info 4.11.6 --output=json | jq -r '.references.spec.tags[] | select (.name=="cli") | .annotations."io.openshift.build.commit.id"'
+      # tag: => "v4.11.7", # oc adm release info 4.11.7 --output=json | jq -r '.references.spec.tags[] | select (.name=="cli") | .annotations."io.openshift.build.commit.id"'
       revision: "262ac9c4cb45930572030d4435a529911f9c3a17"
-  version "4.11.6"
+  version "4.11.7"
   head "https://github.com/openshift/oc.git",
        shallow: false
 
