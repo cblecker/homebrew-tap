71e816ddcaabdce5a51fc6ffe1299cd1614b294f
Christoph Blecker <admin@toph.ca>
2023-03-27 14:34:47 -0700

    oc-console 1.3.0

--- Formula/oc-console.rb
+++ Formula/oc-console.rb
@@ -2,8 +2,8 @@ class OcConsole < Formula
   desc "Oc plugin to open the OpenShift 4 console in your web browser"
   homepage "https://github.com/cblecker/oc-console/"
   url "https://github.com/cblecker/oc-console.git",
-      tag:      "v1.2.1",
-      revision: "8d01b0e9d7a1584141d44d48324a163a492e1c9f"
+      tag:      "v1.3.0",
+      revision: "25fc8d8cf28e87a10bffe7fc26a1c44387bb916d"
   head "https://github.com/cblecker/oc-console.git", branch: "main"
 
   depends_on "go" => :build
@@ -16,7 +16,7 @@ class OcConsole < Formula
     # Create bin directory, as goreleaser doesn't do this
     mkdir bin
 
-    args = ["--rm-dist", "--single-target"]
+    args = ["--clean", "--single-target"]
     args << "--snapshot" if build.head?
     system "goreleaser", "build", *args, "--output=#{bin}/oc-console"
   end
