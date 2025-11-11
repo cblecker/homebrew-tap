56b16b23e3cd882afc9d1bdd736f53c499e0ee49
Christoph Blecker <admin@toph.ca>
2025-11-11 13:56:36 -0800

    oc-console 1.5.1

--- Formula/oc-console.rb
+++ Formula/oc-console.rb
@@ -2,8 +2,8 @@ class OcConsole < Formula
   desc "Oc plugin to open the OpenShift 4 console in your web browser"
   homepage "https://github.com/cblecker/oc-console/"
   url "https://github.com/cblecker/oc-console.git",
-      tag:      "v1.5.0",
-      revision: "3b7766ec188d86ded9fecc5962f534c18bd051c6"
+      tag:      "v1.5.1",
+      revision: "778fd9cc999345ad1f1645783bc34b6a475ab346"
   head "https://github.com/cblecker/oc-console.git", branch: "main"
 
   depends_on "go" => :build
